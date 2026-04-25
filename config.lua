a09608dde45b03af9382ec1673a6e7437e996caa
Itzkeating <ciankeating1904@gmail.com>
2026-04-25 22:10:56 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.Webhook = ""
+
+Config.Website = "https://caldekx.xyz/"
+Config.Discord = "https://discord.gg/veyra"
+
+Config.Logo = "https://cdn.discordapp.com/icons/1377993354821894216/bb62a0a200f742109baa9f7d33a57265.webp?size=1024"
+Config.BanReason = "Veyra | Blacklisted language detected"
+
+
+Config.Mode = "kick" -- you ban also set it to kick if you dont want it to ban players.
+
+Config.BlacklistedWords = {
+    "nigger", 
+    "faggot", 
+    "retard",
+    "fagg0t"
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Veyra Blacklist System'
+shared_script 'config.lua'
+server_script 'server.lua'
